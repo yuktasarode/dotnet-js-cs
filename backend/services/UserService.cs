a5723e7627ccf27aa35103fbf1e9f1fd5a75c948
Yukta Sarode <yuktasarode.sarode@gmail.com>
2026-03-24 14:17:21 +0530

    updated code.

--- backend/services/UserService.cs
+++ backend/services/UserService.cs
@@ -2,6 +2,18 @@ using System.Text.Json;
 
 public class UserService : IUserService
 {
+
+    private readonly List<User> _users = new()
+    {
+        new User { Id = 1, Name = "John Doe", FirstName = "John", LastName = "Doe" },
+        new User { Id = 2, Name = "Jane Doe", FirstName = "Jane", LastName = "Doe" },
+        new User { Id = 3, Name = "Alice Smith", FirstName = "Alice", LastName = "Smith" },
+        new User { Id = 4, Name = "Bob Brown", FirstName = "Bob", LastName = "Brown" },
+        new User { Id = 5, Name = "Charlie Davis", FirstName = "Charlie", LastName = "Davis" }
+    };
+
+
+
     public async Task<List<User>> GetUsers(string? firstName, string? lastName)
     {
         using var client = new HttpClient(); // using disposes cclient variable, just like try , finally. use where you own it. ideally this should be in a httpfactory
@@ -9,6 +21,7 @@ public class UserService : IUserService
         var json = await client.GetStringAsync("https://api.example.com/users");
 
         var users = JsonSerializer.Deserialize<List<User>>(json) ?? new List<User>();
+        var users = _users.AsQueryable();
 
         // Apply filters in memory
         if (!string.IsNullOrEmpty(firstName))
@@ -17,7 +30,10 @@ public class UserService : IUserService
         if (!string.IsNullOrEmpty(lastName))
             users = users.Where(u => u.LastName == lastName).ToList();
 
-        return users.OrderBy(u => u.Name).ToList();
+
+        return await Task.FromResult(users.ToList());
+
+        // return users.OrderBy(u => u.Name).ToList();
     }
 
     public async Task<User?> GetUserById(int id)
---
+++ readme.md
@@ -0,0 +1,7 @@
+Run Dot Net:
+dotnet run
+http://localhost:5062
+
+Run Frontend:
+python3 -m http.server 5500
+http://localhost:5500
